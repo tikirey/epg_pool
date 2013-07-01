591e67a0af29662a243ebb041885594c478d9289
Anton Sekatski <reterius@gmail.com>
2013-07-02 01:10:44 +0400

    Add columns-rows joining function

--- lib/epg_pool.ex
+++ lib/epg_pool.ex
@@ -14,7 +14,28 @@ defmodule EpgPool do
   def sq(pool, sql) do
     :poolboy.transaction(pool, 
       fn(worker) -> 
-        :gen_server.call(worker, {:squery, sql}) 
+        {:ok, columns, rows} = :gen_server.call(worker, {:squery, sql})
+
+        cr_to_keyword_list(columns, rows)
       end)
   end
+
+  defp cr_to_keyword_list(columns, rows) do
+    # Right to left - keep order
+    List.foldr(rows, [], fn (row, acc) -> 
+
+      # Join columns names with actual data
+      { _, result } = Enum.reduce(columns, {0, []}, fn(column, {count, acc}) ->
+
+        key = binary_to_atom( elem(column, 1) )
+
+        value = elem( row, count )
+
+        { count + 1, Keyword.put(acc, key, value) }
+
+      end)
+
+      acc ++ [result]
+    end)
+  end # cr_to_keyword_list(columns, rows)
 end
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EpgPool.Mixfile do
 
   def project do
     [ app: :epg_pool,
-      version: "0.0.1",
+      version: "0.0.2",
       deps: deps ]
   end
 
